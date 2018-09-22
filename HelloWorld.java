c6b202dcd9dd341e5d483d5e67343fef202424be
KhoaMac <leondk237@gmail.com>
2018-09-22 09:43:33 +0700

    tdtu branch revision 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args{
-        System.out.println("Hello World I am Mac Cu Dang Khoa");
+        System.out.println("Hello World I am TDTU");
     }
 }
