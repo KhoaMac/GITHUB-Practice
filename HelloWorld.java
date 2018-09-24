80a632653cfd06ae08013244b7c21ef8dc900207
Mạc Cự Đăng Khoa <37371013+KhoaMac@users.noreply.github.com>
2018-09-24 13:48:14 +0700

    master branch revision 4

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-public class HelloWorld {
-<<<<<<< HEAD
-
-    public static void main(String[] args) {
-        System.out.println("Hello World I am Khoa Mac Cu Dang");
-    }
-
 }
     public static void main(String[] args{
          Scanner sc = new Scanner(System.in);
