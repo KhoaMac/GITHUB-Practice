d2c92e5a28a6b812ce3ed6a766856367c70cac14
Mạc Cự Đăng Khoa <37371013+KhoaMac@users.noreply.github.com>
2018-09-24 20:46:32 +0700

    Count = 0

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 }
     public static void main(String[] args{
-         Scanner sc = new Scanner(System.in);
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
         System.out.print("Input student list: ");
         File file = new File(sc.nextLine());
         sc = new Scanner(file);
