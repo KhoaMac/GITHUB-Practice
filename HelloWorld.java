3f7cc0ec1e6bb8bdc3e4500aec7b20ba2a4b3d2a
Mạc Cự Đăng Khoa <37371013+KhoaMac@users.noreply.github.com>
2018-09-24 20:48:17 +0700

    Total students

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 }
     public static void main(String[] args{
         int count = 0;
+        //Input Student list from file
         Scanner sc = new Scanner(System.in);
         System.out.print("Input student list: ");
-        File file = new File(sc.nextLine());
-        sc = new Scanner(file);
+        File file = new File(sc.nextLine()); 
+        sc = new Scanner(file); // Read file
+        
         System.out.println("Hello World This is TDTU");
         while(sc.hasNextLine()){
             System.out.print("Hello World I am " + sc.nextLine()+ "\n");
             count++;
         }
+        System.out.print("Total student: " + count + " Students");
     }
 }
 
